abe65adad12e3d103d88ba41e73fc99cbe637579
Muhammed Adnan <147593005+muhammedadnanv@users.noreply.github.com>
2024-10-06 09:20:51 +0530

    Update About.jsx

--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -4,7 +4,7 @@ function About() {
   return (
     <div className='px-10 py-10 md:px-20 text-white bg-black'>
         <h1 className='text-3xl font-bold'>What is ComicFix ?</h1>
-        <p className='mt-4'>ComicFix is a platform dedicated to fostering collaboration and growth in the open-source community. We provide developers with real-world projects to contribute to, enhancing their skills while making meaningful impacts. Join us in shaping the future of software development!</p>
+        <p className='mt-4'>ComicFix is a platform dedicated to fostering collaboration and growth in the open-source community. We provide freshers and self-taught developers with the opportunity to work on live, real-world projects, enhancing their skills while making meaningful impacts—all for free. Participants can build their portfolios, earn industry-recognized certifications, and collaborate with a supportive community of peers and mentors. Join us in shaping the future of software development and contributing to impactful open-source projects!</p>
     </div>
   )
 }
