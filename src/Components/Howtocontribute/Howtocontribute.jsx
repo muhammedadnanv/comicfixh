43509376b25782af998581f524ab6c2f29323d27
Muhammed Adnan <147593005+muhammedadnanv@users.noreply.github.com>
2024-10-08 18:13:42 +0530

    Update Howtocontribute.jsx

--- src/Components/Howtocontribute/Howtocontribute.jsx
+++ src/Components/Howtocontribute/Howtocontribute.jsx
@@ -3,91 +3,131 @@ import React from 'react';
 function HowToContribute() {
   return (
     <div className='bg-black'>
-    <h1 className='text-3xl font-bold text-center pt-10 text-white'>How to Contribute ?</h1>
-    <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical bg-black px-10 py-4 text-white">
-      <li>
-        <div className="timeline-middle">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className="h-5 w-5 text-white">
-            <path
-              fillRule="evenodd"
-              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-              clipRule="evenodd" />
-          </svg>
-        </div>
-        <div className="timeline-start mb-10 md:text-end">
-          <time className="font-mono italic">Step 1</time>
-          <div className="text-lg font-black">Fork the Repository</div>
-          Start by forking the project repository to your own GitHub account. This allows you to freely make changes without affecting the main codebase.
-        </div>
-        <hr />
-      </li>
-      <li>
-        <hr />
-        <div className="timeline-middle">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className="h-5 w-5  text-white">
-            <path
-              fillRule="evenodd"
-              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-              clipRule="evenodd" />
-          </svg>
-        </div>
-        <div className="timeline-end mb-10">
-          <time className="font-mono italic">Step 2</time>
-          <div className="text-lg font-black">Make Your Changes</div>
-          Work on your changes locally by cloning the forked repository and modifying the code as needed. Ensure that your changes align with the project guidelines.
-        </div>
-        <hr />
-      </li>
-      <li>
-        <hr />
-        <div className="timeline-middle">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className="h-5 w-5  text-white">
-            <path
-              fillRule="evenodd"
-              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-              clipRule="evenodd" />
-          </svg>
-        </div>
-        <div className="timeline-start mb-10 md:text-end">
-          <time className="font-mono italic">Step 3</time>
-          <div className="text-lg font-black">Test Your Changes</div>
-          Before submitting, ensure that you test your changes thoroughly. Run the existing tests and add new ones if necessary to validate your contributions.
-        </div>
-        <hr />
-      </li>
-      <li>
-        <hr />
-        <div className="timeline-middle">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className="h-5 w-5  text-white">
-            <path
-              fillRule="evenodd"
-              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-              clipRule="evenodd" />
-          </svg>
-        </div>
-        <div className="timeline-end mb-10">
-          <time className="font-mono italic">Step 4</time>
-          <div className="text-lg font-black ">Submit a Pull Request</div>
-          Once you're satisfied with your changes, submit a pull request to the original repository. Be sure to include a clear description of your changes and why they should be merged.
-        </div>
-      </li>
-    </ul>
+      <h1 className='text-3xl font-bold text-center pt-10 text-white'>How to Contribute ?</h1>
+      <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical bg-black px-10 py-4 text-white">
+        <li>
+          <div className="timeline-middle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="h-5 w-5 text-white">
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                clipRule="evenodd" />
+            </svg>
+          </div>
+          <div className="timeline-start mb-10 md:text-end">
+            <time className="font-mono italic">Step 1</time>
+            <div className="text-lg font-black">Fork the Repository</div>
+            Start by forking the project repository to your own GitHub account. This allows you to freely make changes without affecting the main codebase.
+          </div>
+          <hr />
+        </li>
+        <li>
+          <hr />
+          <div className="timeline-middle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="h-5 w-5  text-white">
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                clipRule="evenodd" />
+            </svg>
+          </div>
+          <div className="timeline-end mb-10">
+            <time className="font-mono italic">Step 2</time>
+            <div className="text-lg font-black">Make Your Changes</div>
+            Work on your changes locally by cloning the forked repository and modifying the code as needed. Ensure that your changes align with the project guidelines.
+          </div>
+          <hr />
+        </li>
+        <li>
+          <hr />
+          <div className="timeline-middle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="h-5 w-5  text-white">
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                clipRule="evenodd" />
+            </svg>
+          </div>
+          <div className="timeline-start mb-10 md:text-end">
+            <time className="font-mono italic">Step 3</time>
+            <div className="text-lg font-black">Test Your Changes</div>
+            Before submitting, ensure that you test your changes thoroughly. Run the existing tests and add new ones if necessary to validate your contributions.
+          </div>
+          <hr />
+        </li>
+        <li>
+          <hr />
+          <div className="timeline-middle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="h-5 w-5  text-white">
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                clipRule="evenodd" />
+            </svg>
+          </div>
+          <div className="timeline-end mb-10">
+            <time className="font-mono italic">Step 4</time>
+            <div className="text-lg font-black ">Submit a Pull Request</div>
+            Once you're satisfied with your changes, submit a pull request to the original repository. Be sure to include a clear description of your changes and why they should be merged.
+          </div>
+        </li>
+        <li>
+          <hr />
+          <div className="timeline-middle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="h-5 w-5  text-white">
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                clipRule="evenodd" />
+            </svg>
+          </div>
+          <div className="timeline-start mb-10 md:text-end">
+            <time className="font-mono italic">Step 5</time>
+            <div className="text-lg font-black">Send Your Details</div>
+            After submitting the pull request, send an email with the subject "I am contributing to {the project name}" and include your name, phone number, address, LinkedIn ID, GitHub ID, and links to all your tech platforms.
+          </div>
+        </li>
+        <li>
+          <hr />
+          <div className="timeline-middle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="h-5 w-5  text-white">
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                clipRule="evenodd" />
+            </svg>
+          </div>
+          <div className="timeline-end mb-10">
+            <time className="font-mono italic">Step 6</time>
+            <div className="text-lg font-black">Update LinkedIn Experience</div>
+            Finally, add this contribution to your experience section on LinkedIn, mentioning the project and the role you played.
+          </div>
+        </li>
+      </ul>
     </div>
   );
 }
