df6362e33a79485a6db894447aae82ca88a68689
Muhammed Adnan <147593005+muhammedadnanv@users.noreply.github.com>
2024-10-18 16:12:51 +0530

    Update About.jsx

--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -4,7 +4,7 @@ function About() {
   return (
     <div className='px-10 py-10 md:px-20 text-white bg-black'>
         <h1 className='text-3xl font-bold'>What is ComicFix ?</h1>
-        <p className='mt-4'>ComicFix is an innovative platform based in Kozhikode, Kerala, designed to empower freshers and self-taught developers by offering the chance to work on live projects and earn industry-recognized certifications completely free of charge. Launched in 2023, ComicFix provides invaluable hands-on experience in a real-world development environment, allowing participants to enhance their skills, build impressive portfolios, and collaborate within a supportive community of peers and mentors. Join us at ComicFix to take your development journey to the next level!</p>
+        <p className='mt-4'>ComicFix offers live projects for freshers and self-taught developers to contribute to and earn certifications, completely free of charge. This platform provides an invaluable opportunity to gain hands-on experience in a real-world development environment.</p>
     </div>
   )
 }
